e9c635856ad63534ade33a444623b97a7d1c0144
James Weston <james@fe0.uk>
2015-02-20 17:06:19 +0000

    PowerBackup for Veeam

---
+++ DoBackup.bat
@@ -0,0 +1,26 @@
+@ECHO OFF
+
+ECHO ****** Check for drive *******
+if not exist DESTDRIVE:\Run743584.id (
+C:\PowerBackup\blat -body "Veeam USB Backup Faild Wrong Drive" -to toaddress@meamod.com -f fromaddress@meamod.com -s "Veeam USB Backup Faild Wrong Drive" -server mailserver.meamod.com
+Exit
+)
+
+ECHO ****** Staring RoboCopy ******
+Robocopy SOURCEDRIVE: DESTDRIVE: /e /copyall /r:5 /W:1 /mir /log:"C:\PowerBackup\Log.txt" /xd "System Volume Information" "$RECYCLE.BIN"
+
+
+Echo ****** Sleeping for Eject ******
+
+C:\PowerBackup\sleep -m 60000
+
+
+Echo ****** Ejecting Drive ******
+
+C:\PowerBackup\deveject -EjectId:"USB\VID_0BC2&PID_3320\NA4LCLMD"
+C:\PowerBackup\deveject -EjectId:"USB\VID_0BC2&PID_3320\NA4MGRGP"
+
+
+Echo ****** Sending Notification E-Mail ******
+
+C:\PowerBackup\blat -body "Veeam USB Backup Complete" -to toaddress@meamod.com -f fromaddress@meamod.com -s "Veeam Backup to USB Complete" -server mailserver.meamod.com -attacht "C:\PowerBackup\Log.txt","C:\PowerBackup\PowerLog.txt"
